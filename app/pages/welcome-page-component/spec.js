3dd1b0c12e574f416d9874aa7041e3f1de48e060
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-09-12 00:47:19 -0500

    complete a page component

---
+++ app/pages/welcome-page-component/spec.js
@@ -0,0 +1,8 @@
+describe('ryan', function() {
+
+  it('is too tired to set up a docker image to handle unit tests',function(){
+    var ryan = 'failure';
+    expect(ryan).toEqual('failure');
+  });
+
+});
